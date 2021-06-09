f2dc6b5ca5373a323704ba1c9fd84258fab7c386
Alex <74212502+bluzerone@users.noreply.github.com>
2021-06-10 01:22:33 +0300

    Created a function to send a request to the Flickr server to receive a response containing an array of photos

--- src/app/shared/flickr.service.ts
+++ src/app/shared/flickr.service.ts
@@ -1,11 +1,34 @@
+import { FlickrOutput } from './../models/flickr-output';
+import { FlickrPhoto } from './../models/flickr-photo';
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { debounceTime, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FlickrService {
+  searchInputValue: string;
 
-  public searchText: String = ''
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  search_keyword(keyword: string){
+    const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
+    const params = `api_key=${environment.flickr.key}&text=${keyword}&format=json&nojsoncallback=1&per_page=40`;
+
+    return this.http.get(url+params).pipe(map((res: FlickrOutput) => {
+      const urlArray = [];
+      res.photos.photo.forEach((photo: FlickrPhoto) => {
+        const photoObject = {
+          url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+          title: photo.title
+        };
+        urlArray.push(photoObject)
+      });
+      return urlArray;
+    }));
+  }
+
 }
