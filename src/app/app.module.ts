316904412acaecf3737eac260481736da53c8509
Alex <74212502+bluzerone@users.noreply.github.com>
2021-06-10 01:24:20 +0300

    the modules necessary for the project have been imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { AngularFireModule } from "@angular/fire";
@@ -7,18 +8,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from 'src/environments/environment';
 
+import { TruncateModule } from 'ng2-truncate';
+import {NgxPaginationModule} from 'ngx-pagination';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { FooterComponent } from './footer/footer.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { SearchComponent } from './search/search.component';
-
 
 @NgModule({
   declarations: [
@@ -30,6 +36,7 @@ import { SearchComponent } from './search/search.component';
     SearchComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
@@ -38,7 +45,12 @@ import { SearchComponent } from './search/search.component';
     AngularFirestoreModule,
     AngularFireStorageModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    TruncateModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
