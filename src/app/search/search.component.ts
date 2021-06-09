6676ca7e3d673311ac8f437b4074b41c8105735f
Alex <74212502+bluzerone@users.noreply.github.com>
2021-06-10 01:28:51 +0300

    Implemented the logic of receiving photos from Flickr, at the user's request, as well as saving to bookmarks. Styled SearchComponent

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,13 @@
+import { Bookmark } from './../models/bookmark';
+import { BaseService } from './../shared/base.service';
+import { FlickrPhoto } from './../models/flickr-photo';
 import { Component, OnInit } from '@angular/core';
 import { FlickrService } from '../shared/flickr.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { MainService } from './../shared/main.service';
+import { TruncateModule } from 'ng2-truncate';
+
+
 
 @Component({
   selector: 'app-search',
@@ -11,10 +17,17 @@ import { MainService } from './../shared/main.service';
 export class SearchComponent implements OnInit {
 
   urlSubscription: any;
+  images: FlickrPhoto[] = [];
+  searchKeywords: string = '';
+  message:boolean = false;
+  totalLength: number;
+  page: number = 1;
+
 
   constructor(public flickrService: FlickrService,
      private route : ActivatedRoute,
-     public mainService: MainService) { }
+     public mainService: MainService,
+     public baseService: BaseService) { }
 
   ngOnInit(): void {
   this.urlSubscription = this.route.url.subscribe(data => {
@@ -28,12 +41,38 @@ export class SearchComponent implements OnInit {
       links.active = true
     }
     })
-  })
+  });
+  this.totalLength = this.images.length;
+  }
+
+  search(event: any){
+    this.searchKeywords = event.target.value.toLowerCase();
+    if(this.searchKeywords && this.searchKeywords.length > 0){
+      this.flickrService.search_keyword(this.searchKeywords).toPromise()
+      .then(res => {
+        this.images = res;
+        console.log(this.images);
+
+      });
+    } else if (this.searchKeywords.length === 0 ){
+      this.message = true;
+      this.images = [];
+    }
+  }
+
+
+
+  createBookmarkObj(url, title){
+    const bookmark: Bookmark = {
+      url,
+      title
+    };
+    this.baseService.setBookmark(bookmark);
+    this.mainService.openSnackBar('photo has been successfully bookmarked!', "right", "bottom", "success-dialog-red");
   }
 
   ngOnDestroy(): void {
     this.urlSubscription.unsubscribe();
   }
-
 
 }
