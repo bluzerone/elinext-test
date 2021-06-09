172f02b15027eb98a45325791ce36cd7b8dc0734
Alex <74212502+bluzerone@users.noreply.github.com>
2021-06-10 01:26:17 +0300

    The BookmarksComponent was created, with the implementation of the logic for receiving an array of photos saved by the user. Bookmark interface created

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -1,6 +1,10 @@
+import { Bookmark } from './../models/bookmark';
+import { BaseService } from './../shared/base.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MainService } from '../shared/main.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-bookmarks',
@@ -10,26 +14,44 @@ import { MainService } from '../shared/main.service';
 export class BookmarksComponent implements OnInit {
 
   urlSubscription: any;
+  bookmarksSubscription: Subscription;
+  bookmarksList: Bookmark[];
+  totalLength: number;
+  page: number = 1;
 
-  constructor(public mainService: MainService, private route : ActivatedRoute) { }
+  constructor(public mainService: MainService,
+              private route : ActivatedRoute,
+              public baseService: BaseService) { }
 
   ngOnInit(): void {
     this.urlSubscription = this.route.url.subscribe(data => {
-      let path:any;
+      let path: any;
       data.forEach(d => {
-        console.log(d.path);
-       path = d.path
+       path = d.path;
       });
       this.mainService.sidenavLinks.forEach(links => {
       if(links.link === path){
-        links.active = true
-      }
-      })
+        links.active = true;
+      };
+      });
     })
+    this.bookmarksSubscription = this.baseService.getBookmarks().subscribe((res) => {
+     this.bookmarksList = res.map(e =>  {
+        return {
+         id: e.payload.doc.id,
+         ...e.payload.doc.data() as {}
+       } as Bookmark;
+     });
+     this.totalLength = this.bookmarksList.length;
+    });
+    this.totalLength = this.bookmarksList.length;
   }
+
+
 
   ngOnDestroy(): void {
     this.urlSubscription.unsubscribe();
+    this.bookmarksSubscription.unsubscribe();
   }
 
 }
