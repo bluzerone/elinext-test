9c8e0394f22a76d5629e88501937e64936af3ada
Alex <74212502+bluzerone@users.noreply.github.com>
2021-06-10 20:51:02 +0300

    Created blank popup-auth component

---
+++ src/app/popup-auth/popup-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-auth',
+  templateUrl: './popup-auth.component.html',
+  styleUrls: ['./popup-auth.component.scss']
+})
+export class PopupAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
