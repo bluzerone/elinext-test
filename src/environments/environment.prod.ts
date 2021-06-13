cd23a50f922c1465a64f69d8e22aad164db7d7f8
Alex <74212502+bluzerone@users.noreply.github.com>
2021-06-14 01:45:07 +0300

    add firebase config to production environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD4f4CJBdQQYWC3HByniVHntdIREDe_vPk",
+    authDomain: "elinext-test-base.firebaseapp.com",
+    projectId: "elinext-test-base",
+    storageBucket: "elinext-test-base.appspot.com",
+    messagingSenderId: "676427949575",
+    appId: "1:676427949575:web:763c720d1c85479165aca2",
+    measurementId: "G-598LEBTWRG"
+  },
+  flickr: {
+    key: 'ab2d69043a2b1a0e2f49c927c85b3435'
+  }
 };
